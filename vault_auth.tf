5d295d54d8abf1a68217b26a47d20288bacd3b6e
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-03-16 23:16:00 -0400

    :bug:(vault/auth): using  to login

--- vault_auth.tf
+++ vault_auth.tf
@@ -1,33 +1,20 @@
 provider "vault" {
   address = "https://vault.keegan.boston"
+  token   = vault_approle_auth_backend_login.approle.token
 }
 
-resource "vault_auth_backend_login" "approle" {
-  path      = "auth/system:core/login"
-
-  parameters = {
-    role_id   = var.vault_role_id
-    secret_id = var.vault_secret_id
-  }
+# Authenticate with Vault using AppRole
+resource "vault_approle_auth_backend_login" "approle" {
+  role_id   = var.vault_role_id
+  secret_id = var.vault_secret_id
 }
 
+# Retrieve secrets from Vault
 data "vault_kv_secret_v2" "myapp_secrets" {
-  mount = "secret"
-  name  = "myapp"
+  mount = "core"
+  name  = "core"
 
-  depends_on = [vault_auth_backend_login.approle]
-}
-
-resource "docker_container" "ubuntu" {
-  name  = "ubuntu-secrets-container"
-  image = "ubuntu:latest"
-
-  env = [
-    "DB_USER=${data.vault_kv_secret_v2.myapp_secrets.data["DB_USER"]}",
-    "DB_PASS=${data.vault_kv_secret_v2.myapp_secrets.data["DB_PASS"]}"
-  ]
-
-  command = ["sleep", "infinity"] # Keep the container running for testing
+  depends_on = [vault_approle_auth_backend_login.approle]
 }
 
 variable "vault_role_id" {
