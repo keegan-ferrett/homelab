f1d1da13d6b7d327bd6e7871e16c735e7fb90e41
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-03-16 23:26:50 -0400

    :sparkles:(vault/auth): logging into vault to access secrets (#11)
    * :sparkles:(vault/auth): logging into vault to access secrets
    
    * :bug:(): updated workflow to use secrets context
    
    * :bug:(vault/auth): using  to login
    
    * :bug:(vault/auth): fixing dep cycle
    
    * :sparkles:(): testing my vault secrets

---
+++ vault_auth.tf
@@ -0,0 +1,41 @@
+provider "vault" {
+  address = "https://vault.keegan.boston"
+}
+
+# Authenticate with Vault using AppRole
+resource "vault_approle_auth_backend_login" "approle" {
+  role_id   = var.vault_role_id
+  secret_id = var.vault_secret_id
+}
+
+# Retrieve secrets from Vault
+data "vault_kv_secret_v2" "postgres" {
+  mount = "core"
+  name  = "postgres/admin"
+
+  depends_on = [vault_approle_auth_backend_login.approle]
+}
+
+# Deploy Ubuntu container with secrets as environment variables
+resource "docker_container" "ubuntu" {
+  name  = "ubuntu-secrets-container"
+  image = "ubuntu:latest"
+
+  env = [
+    "DB_USER=${data.vault_kv_secret_v2.postgres.data["username"]}",
+    "DB_PASS=${data.vault_kv_secret_v2.postgres.data["password"]}"
+  ]
+
+  command = ["sleep", "infinity"] # Keep the container running
+}
+
+variable "vault_role_id" {
+  type        = string
+  description = "Vault AppRole Role ID"
+}
+
+variable "vault_secret_id" {
+  type        = string
+  description = "Vault AppRole Secret ID"
+  sensitive   = true
+}
