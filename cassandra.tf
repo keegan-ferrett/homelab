d4df86bf20878dc929107d28136afca50c2598fc
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-03-21 15:05:23 -0400

    :sparkles:(cassandra): setting up initial core cassandra instance (#14)
    * :sparkles:(cassandra): setting up initial core cassandra instance
    
    * :bug:(cassandra): setup in the correct tf file

---
+++ cassandra.tf
@@ -0,0 +1,24 @@
+resource "docker_image" "cassandra" {
+  name = "cassandra:4.0"
+}
+
+resource "docker_container" "cassandra" {
+  name          = "cassandra-core"
+  image         = docker_image.cassandra.image_id
+  network_mode  = "bridge"
+
+  networks_advanced {
+    name            = docker_network.core_network.id
+  }
+
+  volumes {
+    host_path       = "/data/cassandra"
+    container_path  = "/var/lib/cassandra"
+    read_only       = false
+  }
+
+  ports {
+    internal = 7000
+    external = 7000
+  }
+}
