0bd4dac56d6ad4040aeb33647863eaef4e5607d8
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-03-21 14:50:42 -0400

    :bug:(cassandra): setup in the correct tf file

--- postgres.tf
+++ postgres.tf
@@ -1,24 +1,44 @@
-resource "docker_image" "cassandra" {
-  name = "cassandra:4.0"
+resource "docker_image" "postgres" {
+  name = "postgres:17.2"
 }
 
-resource "docker_container" "cassandra" {
-  name          = "cassandra-core"
+data "vault_kv_secret_v2" "postgres" {
+  mount = "core"
+  name  = "postgres/admin"
+}
+
+resource "docker_container" "postgres" {
+  name          = "postgres-core"
   image         = docker_image.postgres.image_id
   network_mode  = "bridge"
+  env           = [
+    "POSTGRES_USER=${data.vault_kv_secret_v2.postgres.data["username"]}",
+    "POSTGRES_PASSWORD=${data.vault_kv_secret_v2.postgres.data["password"]}"
+  ]
 
   networks_advanced {
     name            = docker_network.core_network.id
   }
 
   volumes {
-    host_path       = "/data/cassandra"
-    container_path  = "/var/lib/cassandra"
+    host_path       = "/data/postgres"
+    container_path  = "/var/lib/postgresql/data"
     read_only       = false
   }
 
   ports {
-    internal = 7000
-    external = 7000
+    internal = 5432
+    external = 5432
   }
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.tcp.routers.postgres-router.rule"
+    value = "Host(\"postgres.keegan.boston\")"
+  }
+
 }
