988545e1429dfdbd9d17750053e7fafdeaf1ca84
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-03-21 14:44:04 -0400

    :sparkles:(cassandra): setting up initial core cassandra instance

--- postgres.tf
+++ postgres.tf
@@ -1,44 +1,24 @@
-resource "docker_image" "postgres" {
-  name = "postgres:17.2"
+resource "docker_image" "cassandra" {
+  name = "cassandra:4.0"
 }
 
-data "vault_kv_secret_v2" "postgres" {
-  mount = "core"
-  name  = "postgres/admin"
-}
-
-resource "docker_container" "postgres" {
-  name          = "postgres-core"
+resource "docker_container" "cassandra" {
+  name          = "cassandra-core"
   image         = docker_image.postgres.image_id
   network_mode  = "bridge"
-  env           = [
-    "POSTGRES_USER=${data.vault_kv_secret_v2.postgres.data["username"]}",
-    "POSTGRES_PASSWORD=${data.vault_kv_secret_v2.postgres.data["password"]}"
-  ]
 
   networks_advanced {
     name            = docker_network.core_network.id
   }
 
   volumes {
-    host_path       = "/data/postgres"
-    container_path  = "/var/lib/postgresql/data"
+    host_path       = "/data/cassandra"
+    container_path  = "/var/lib/cassandra"
     read_only       = false
   }
 
   ports {
-    internal = 5432
-    external = 5432
+    internal = 7000
+    external = 7000
   }
-
-  labels {
-    label = "traefik.enable"
-    value = "true"
-  }
-
-  labels {
-    label = "traefik.tcp.routers.postgres-router.rule"
-    value = "Host(\"postgres.keegan.boston\")"
-  }
-
 }
