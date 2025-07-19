1874b1d4d93f86e68e8e7f99147ab3d2f79372d4
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-07-19 18:30:39 -0400

    :sparkles:(redis): adding redis service

---
+++ services/redis.tf
@@ -0,0 +1,36 @@
+resource "docker_image" "redis" {
+  name = "redis:8.0"
+}
+
+resource "docker_container" "redis" {
+  name          = "redis-core"
+  image         = docker_image.redis.image_id
+  network_mode  = "bridge"
+  env           = [
+  ]
+
+  networks_advanced {
+    name            = "e6e3be88c519"
+  }
+
+  volumes {
+    host_path       = "/data/redis"
+    container_path  = "/data"
+    read_only       = false
+  }
+
+  ports {
+    internal = 6379
+    external = 6379
+  }
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.tcp.routers.redis-router.rule"
+    value = "HostSNI(\"redis.keegan.boston\")"
+  }
+}
