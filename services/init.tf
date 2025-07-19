1ad24434b2a7be44c9c6005e4bc926de0a66a7a4
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-07-19 15:40:49 -0400

    :bug:(postgres): using HostSNI for tcp traefik config

--- services/init.tf
+++ services/init.tf
@@ -113,7 +113,7 @@ resource "docker_container" "postgres" {
 
   labels {
     label = "traefik.tcp.routers.postgres-router.rule"
-    value = "Host(\"postgres.keegan.boston\")"
+    value = "HostSNI(\"postgres.keegan.boston\")"
   }
 }
 
@@ -125,7 +125,7 @@ resource "consul_service" "postgres" {
   tags    = ["db", "static"]
 
   check {
-    check_id       = "postgres_check"
+    check_id = "postgres_check"
     name     = "Postgres TCP Check"
     tcp      = "192.168.88.101:5432"
     interval = "10s"
