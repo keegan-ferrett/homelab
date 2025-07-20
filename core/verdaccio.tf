c092f89193b13e415cbf1b5f253d3198392f8682
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-07-20 14:41:43 -0400

    :sparkles:(npm): added local npm registry

---
+++ core/verdaccio.tf
@@ -0,0 +1,48 @@
+resource "docker_image" "verdaccio" {
+  name = "verdaccio/verdaccio:4"
+}
+
+resource "docker_container" "verdaccio" {
+  name          = "verdaccio-core"
+  image         = docker_image.verdaccio.image_id
+
+  networks_advanced {
+    name            = docker_network.core_network.id
+  }
+
+  volumes {
+    host_path       = "/data/verdaccio"
+    container_path  = "/verdaccio/storage"
+    read_only       = false
+  }
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.verdaccio-internal-router.rule"
+    value = "Host(\"npm.keegan.boston\")"
+  }
+
+  labels {
+    label = "traefik.http.routers.verdaccio-internal-router.entrypoints"
+    value = "websecure"
+  }
+
+  labels {
+    label = "traefik.http.routers.verdaccio-internal-router.tls"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.verdaccio-internal-router.tls.certresolver"
+    value = "myresolver"
+  }
+
+  labels {
+    label = "traefik.http.services.verdaccio-service.loadbalancer.server.port"
+    value = "4873"
+  }
+}
