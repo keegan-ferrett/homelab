f816232e68479e68623a0283994a7b8cda416eb1
Keegan Ferrett <keegan.ferrett@gmail.com>
2025-07-20 00:11:35 -0400

    :sparkles:(registry): adding docker registry service

---
+++ core/registry.tf
@@ -0,0 +1,53 @@
+resource "docker_image" "registry" {
+  name = "registry:3"
+}
+
+resource "docker_container" "registry" {
+  name          = "registry-core"
+  image         = docker_image.registry.image_id
+
+  networks_advanced {
+    name            = docker_network.core_network.id
+  }
+
+  volumes {
+    host_path       = "/data/registry"
+    container_path  = "/mnt/registry"
+    read_only       = false
+  }
+
+  ports {
+    internal = 5000
+    external = 5000
+  }
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.registry-internal-router.rule"
+    value = "Host(\"docker.keegan.boston\")"
+  }
+
+  labels {
+    label = "traefik.http.routers.registry-internal-router.entrypoints"
+    value = "websecure"
+  }
+
+  labels {
+    label = "traefik.http.routers.registry-internal-router.tls"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.registry-internal-router.tls.certresolver"
+    value = "myresolver"
+  }
+
+  labels {
+    label = "traefik.http.services.registry-service.loadbalancer.server.port"
+    value = "5000"
+  }
+}
